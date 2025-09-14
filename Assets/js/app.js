de0dd43ec668f26557f5e3542575f68176dae77d
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-09-13 21:44:46 -0700

    mobile menu

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,11 +1,18 @@
 // mobile menu links active 
-const menuLinks = document.querySelectorAll("header nav ul li");
+const menuLinks = document.querySelectorAll(".mob_nav-item");
+const shopItem = document.querySelector(".shop-item");
 
 menuLinks.forEach((link) => {
   link.addEventListener("click", () =>{
-    menuLinks.forEach(i => i.classList.remove("active"));
-    link.classList.add("active")
-  })
+    menuLinks.forEach(i => {
+      i.classList.remove("active");
+      if(shopItem.classList.contains("active")){
+        submenu.classList.remove("open");
+        chevron.classList.remove("rotate-180");
+      }
+    });
+    link.classList.add("active");
+  });
 })
 
 // disable links 
@@ -16,3 +23,15 @@ links.forEach((link) => {
     e.preventDefault()
   })
 })
+
+// open shop sub menu 
+const shopBtn = document.querySelector(".mob_nav-item > button");
+const submenu = document.querySelector(".mob_shop-sub");
+const chevron = shopBtn.querySelector("svg:last-child");
+
+shopBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  submenu.classList.toggle("open");
+  chevron.classList.toggle("rotate-180");
+});
+
